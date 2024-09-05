ee6643e2a59d0cfed65b7717508c98daf161ac28
Tássia Martins <tassiamaggv929@hotmail.com>
2024-09-05 00:18:35 -0300

    feat(routes):  atualiza as rotas para suas respectivas controllers

---
+++ taverna_do_dragao/app/Http/Controllers/OrderController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\OrderRequest;
+use Illuminate\Http\Request;
+use App\Models\Item;
+use App\Models\Order;
+use App\Models\Product;
+
+class OrderController extends Controller
+{
+    public function deleteOrder(Request $request)
+    {
+        Order::destroy($request->id);
+        session()->flash('message', "Comanda deletada com sucesso!");
+        return to_route('dashboard.order.index');
+    }
+
+    public function viewOrder(Request $request)
+    {
+        // $order = Order::findOrFail($request->id);
+        $order = Order::with('Product')->find($request->id);
+        $totalPrice = 0;
+        foreach ($order->Product as $product) {
+            $totalPrice = $totalPrice + ((float) $product->price * $product->pivot->quantity);
+        }
+        return view('dashboard.order.view')->with(compact('order', 'totalPrice'));
+    }
+
+    public function updateOrder(Request $request)
+    {
+        $user_id = auth()->user()->id;
+
+        $order = Order::findOrFail($request->id);
+        $order->customerName = $request->input('customerName');
+        $order->customerPhone = $request->input('customerPhone');
+        $order->tableNumber = $request->input('tableNumber');
+        $order->user_id = $user_id;
+
+        $order->save();
+
+        if ($order->save()) {
+            return to_route('dashboard.order.index')->with('message', "A comanda foi editada com sucesso");;
+        }
+        return to_route("dashboard.index");
+    }
+
+    public function indexOrder()
+    {
+        $orders = Order::query()->orderBy('tableNumber')->get();
+        error_log($orders);
+        return view('dashboard.order.index')->with(compact('orders'));
+    }
+
+    public function createOrder()
+    {
+        return view('dashboard.order.create');
+    }
+
+    public function storeOrder(OrderRequest $request)
+    {
+        $user_id = auth()->user()->id;
+
+        $request->validated();
+
+        $order = new Order();
+        $order->customerName = $request->input('customerName');
+        $order->customerPhone = $request->input('customerPhone');
+        $order->tableNumber = $request->input('tableNumber');
+        $order->user_id = $user_id;
+
+        if ($order->save()) {
+            return to_route(("dashboard.order.index"))->with('message', "A comanda foi cadastrada com sucesso");
+        }
+        return to_route("dashboard.index");
+    }
+    
+    public function indexAddProduct(Request $request)
+    {
+        $order = Order::findOrFail($request->id);
+        $products = Product::query()->orderBy('name')->get();
+        return view('dashboard.order.addProduct')->with(compact('products', 'order'));
+    }
+
+    public function indexEdit(Request $request)
+    {
+        $products = Product::query()->orderBy('name')->get();
+        $order = Order::findOrFail($request->id);
+        return view('dashboard.order.edit')->with(compact('products', 'order'));
+    }
+
+}
